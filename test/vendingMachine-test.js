71925f25b277e40174a9d60841d6227a157d9c5a
FelipeGeazi <felipegeaziartes@gmail.com>
2022-06-15 21:37:54 -0300

    Add test SellTokens

--- test/vendingMachine-test.js
+++ test/vendingMachine-test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { waffle } = require('hardhat');
 require("@nomiclabs/hardhat-web3");
 describe('Initializing', () => {
-    
+
     //checar se carregou os valores corretos
     //espera que retorne to total supply
     //espera que retorne o buyingPrice
@@ -13,7 +13,7 @@ describe('Initializing', () => {
     let gamaToken
     let VendingMachine
     let vendingMachine
-    beforeEach(async function () {
+    beforeEach(async function() {
         const [admin, wallets] = await ethers.getSigners();
         GamaToken = await ethers.getContractFactory('GamaToken');
         gamaToken = await GamaToken.deploy(1000);
@@ -22,7 +22,7 @@ describe('Initializing', () => {
         vendingMachine = await VendingMachine.deploy(gamaToken.address);
         vendingMachine.deployed();
     })
-    it('should return the values passed on constructor', async () => {
+    it('should return the values passed on constructor', async() => {
         expect(await vendingMachine.buyingPrice()).to.be.equal(web3.utils.toWei('2', 'ether'));
         expect(await vendingMachine.sellingPrice()).to.be.equal(web3.utils.toWei('1', 'ether'));
         expect(await vendingMachine.availableSupply()).to.be.equal(0);
@@ -40,7 +40,7 @@ describe('BuyTokens', () => {
     let VendingMachine
     let vendingMachine
     const provider = waffle.provider;
-    beforeEach(async function () {
+    beforeEach(async function() {
         [admin, ...wallets] = await ethers.getSigners();
         GamaToken = await ethers.getContractFactory('GamaToken');
         gamaToken = await GamaToken.deploy(1000);
@@ -50,50 +50,93 @@ describe('BuyTokens', () => {
         vendingMachine.deployed();
         await gamaToken.allowAddress(vendingMachine.address)
     })
-    it('should revert if the amount is zero', async () => {
+    it('should revert if the amount is zero', async() => {
         // console.log(await vendingMachine.buyTokens(0))
         await expect(vendingMachine.buyTokens(0)).to.be.revertedWith('Amount has to be greater than 0');
     })
-    it('should revert if the transaction value is lower than the expected', async () => {
+    it('should revert if the transaction value is lower than the expected', async() => {
         await expect(vendingMachine.buyTokens(1, { value: web3.utils.toWei('1', 'ether') })).to.be.revertedWith('The trasaction value was lower than expected');
     })
-    it('should revert if the availabeSupply is less than the amount', async () => {
-        await expect(vendingMachine.buyTokens(1, { value: web3.utils.toWei('2', 'ether')} )).to.be.revertedWith('Not enough tokens in stock');
-    })
-    // it('should be able to buy tokens', async () => {
-    //  // console.log(admin)
-    //  expect(await gamaToken.balanceOf(admin.address)).to.be.equal(1000);
-    //  await vendingMachine.loadTokens(100)
-    //  expect(await gamaToken.balanceOf(admin.address)).to.be.equal(900);
-    //  expect(await provider.getBalance(wallets[0].address)).to.be.equal(web3.utils.toWei('10000', 'ether').toString())
-    //  await vendingMachine.connect(wallets[0]).buyTokens(1, { gasPrice: 0, value: web3.utils.toWei('2', 'ether')} )
-    //  // await vendingMachine.connect(wallets[0]).buyTokens(1, { baseFeePerGas: 0, gasPrice: 0, value: web3.utils.toWei('2', 'ether')} )
-    //  expect(await gamaToken.balanceOf(wallets[0].address)).to.be.equal(1);
-    //  expect(await provider.getBalance(wallets[0].address)).to.be.equal(web3.utils.toWei('9998', 'ether').toString())
-    // })
+    it('should revert if the availabeSupply is less than the amount', async() => {
+            await expect(vendingMachine.buyTokens(1, { value: web3.utils.toWei('2', 'ether') })).to.be.revertedWith('Not enough tokens in stock');
+        })
+        // it('should be able to buy tokens', async () => {
+        //  // console.log(admin)
+        //  expect(await gamaToken.balanceOf(admin.address)).to.be.equal(1000);
+        //  await vendingMachine.loadTokens(100)
+        //  expect(await gamaToken.balanceOf(admin.address)).to.be.equal(900);
+        //  expect(await provider.getBalance(wallets[0].address)).to.be.equal(web3.utils.toWei('10000', 'ether').toString())
+        //  await vendingMachine.connect(wallets[0]).buyTokens(1, { gasPrice: 0, value: web3.utils.toWei('2', 'ether')} )
+        //  // await vendingMachine.connect(wallets[0]).buyTokens(1, { baseFeePerGas: 0, gasPrice: 0, value: web3.utils.toWei('2', 'ether')} )
+        //  expect(await gamaToken.balanceOf(wallets[0].address)).to.be.equal(1);
+        //  expect(await provider.getBalance(wallets[0].address)).to.be.equal(web3.utils.toWei('9998', 'ether').toString())
+        // })
 })
+
+describe('SellTokens', () => {
+
+    //deve retornar erro caso o amount seja  zero
+    //deve retornar erro caso o balanço seja menor do que o esperado
+    //deve transferir os token e aumenta o saldo de ethers
+
+    let admin
+    let wallets
+    let GamaToken
+    let gamaToken
+    let VendingMachine
+    let vendingMachine
+    const provider = waffle.provider;
+    beforeEach(async function() {
+        [admin, ...wallets] = await ethers.getSigners();
+        GamaToken = await ethers.getContractFactory('GamaToken');
+        gamaToken = await GamaToken.deploy(1000);
+        gamaToken.deployed();
+        VendingMachine = await ethers.getContractFactory('VendingMachine');
+        vendingMachine = await VendingMachine.deploy(gamaToken.address);
+        vendingMachine.deployed();
+        await gamaToken.allowAddress(vendingMachine.address)
+    })
+    it('should revert if the amount is zero', async() => {
+        // console.log(await vendingMachine.buyTokens(0))
+        await expect(vendingMachine.sellTokens(0)).to.be.revertedWith('Amount has to be greater than 0');
+    })
+    it('should revert if the transaction value is lower than the expected', async() => {
+        await expect(vendingMachine.sellTokens(1)).to.be.revertedWith('The machine does not have enough Ether');
+    })
+
+
+})
+
+
+
 //deve retornar erro caso o amount seja menor que zero
 //deve retornar erro caso o balanço seja menor do que o esperado
 //deve transferir os token e aumenta o saldo de ethers
+//
 //apenas o admin pode utilizar
 //deve retornar erro caso o balanço de ethers seja zero
 //deve transferir os ethers e aumenta o saldo de ethers
+//
 //apenas o admin pode utilizar
 //deve retornar erro caso o price seja zero
 //deve retornar erro caso o price seja igual ao price atual
 //deve retornar erro caso o pŕice seja maior que o buyingprice
 //deve alterar o preço
+//
 //apenas o admin pode utilizar
 //deve retornar erro caso o price seja zero
 //deve retornar erro caso o price seja igual ao price atual
 //deve retornar erro caso o pŕice seja menor que o buyingprice
 //deve alterar o preço
+//
 //apenas o admin pode utilizar
 //deve retornar erro caso o amount seja zero
 //deve transferir o amount da carteira do admin para o contrato
+//
 //apenas o admin pode utilizar
 //deve retornar erro caso o value seja zero
 //deve transferir o valor de ethers da carteira do admin para o contrato
+//
 //apenas o admin pode utilizar
 //transferir o saldo total de tokens e ethers do contrato para o admin
 //checar se o contrato morreu
